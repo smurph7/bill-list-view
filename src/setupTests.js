27d4126a037d15ab9bffdc5d943a33b232686f0f
Sarah Murphy <sarahnmurphy93@gmail.com>
2020-07-12 09:03:16 +1000

    add enzyme to test state hooks

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,3 +1,7 @@
 import fetchMock from "jest-fetch-mock";
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+Enzyme.configure({ adapter: new Adapter() });
 
 fetchMock.enableMocks();
