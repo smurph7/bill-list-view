579fbda25b5fb9fa6ba51fefdaf536b7586ae2b8
Sarah Murphy <sarahnmurphy93@gmail.com>
2020-07-12 15:16:06 +1000

    refactor BillItem to use state hooks instead of class

--- __tests__/BillItem.js
+++ __tests__/BillItem.js
@@ -1,6 +1,8 @@
-import React from 'react';
+import * as React from 'react';
+import { shallow } from 'enzyme';
+import { TouchableOpacity } from 'react-native';
 import BillItem, { formatString, formatDate } from '../src/components/BillItem';
-import renderer from 'react-test-renderer';
+import BillFullScreenModal from '../src/components/BillFullScreenModal';
 
 jest.mock('@expo/vector-icons', () => {
   const { View } = require('react-native');
@@ -10,6 +12,8 @@ jest.mock('@expo/vector-icons', () => {
 });
 
 describe('BillItem', () => {
+  const setState = jest.fn();
+  const useStateMock = () => [false, setState];
   const bill = {
     id: '1',
     amount: 20,
@@ -19,9 +23,8 @@ describe('BillItem', () => {
     thumbnailUrl: 'https://via.placeholder.com/150/33FFFA?text=School+Fees',
   };
 
-  it('should pass bill props correctly', () => {
-    const tree = renderer.create(<BillItem bill={bill} />);
-    expect(tree.root.props).toEqual({ bill });
+  afterEach(() => {
+    jest.clearAllMocks();
   });
 
   it('should format status to have capital first letter', () => {
@@ -32,26 +35,17 @@ describe('BillItem', () => {
     expect(formatDate('2020-06-21')).toEqual('21/06/2020');
   });
 
-  it('should set isBillFullScreen to true and pass the image url on openImage', () => {
-    const tree = renderer.create(<BillItem bill={bill} />);
-    const instance = tree.getInstance();
-    const image = 'url';
-    jest.spyOn(instance, 'setState');
-    instance.openImage(image);
-    expect(instance.setState).toHaveBeenCalledWith({
-      isBillFullScreen: true,
-      image,
-    });
+  it('should set isBillFullScreen to true on press thumbnail', () => {
+    jest.spyOn(React, 'useState').mockImplementation(useStateMock);
+    const wrapper = shallow(<BillItem bill={bill} />);
+    wrapper.find(TouchableOpacity).props().onPress();
+    expect(setState).toHaveBeenCalledWith(true);
   });
 
-  it('should set isBillFullScreen to false and image to empty string on closeImage', () => {
-    const tree = renderer.create(<BillItem bill={bill} />);
-    const instance = tree.getInstance();
-    jest.spyOn(instance, 'setState');
-    instance.closeImage();
-    expect(instance.setState).toHaveBeenCalledWith({
-      isBillFullScreen: false,
-      image: ''
-    });
+  it('should set isBillFullScreen to false on press close modal', () => {
+    jest.spyOn(React, 'useState').mockImplementation(useStateMock);
+    const wrapper = shallow(<BillItem bill={bill} />);
+    wrapper.find(BillFullScreenModal).props().close();
+    expect(setState).toHaveBeenCalledWith(false);
   });
 });
--- src/components/BillItem.js
+++ src/components/BillItem.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, StyleSheet, Image, TouchableOpacity } from 'react-native';
 import BillFullScreenModal from './BillFullScreenModal';
 import TextField from './TextField';
@@ -13,62 +13,49 @@ export const formatDate = (date) => {
   return date.split('-').reverse().join('/');
 };
 
-export default class BillItem extends React.Component {
-  state = {
-    isBillFullScreen: false,
-    image: '',
-  };
+const BillItem = (props) => {
+  const [isBillFullScreen, setBillFullScreen] = useState(false);
+  const { bill } = props;
+  const closeModal = () => setBillFullScreen(false);
+  const formattedStatus = formatString(bill.status);
+  const formattedDate = formatDate(bill.date);
 
-  openImage = (url) => {
-    this.setState({ isBillFullScreen: true, image: url });
-  };
-
-  closeImage = () => {
-    this.setState({ isBillFullScreen: false, image: '' });
-  };
-
-  render() {
-    const { bill } = this.props;
-    const formattedStatus = formatString(bill.status);
-    const formattedDate = formatDate(bill.date);
-    return (
-      <View style={styles.container}>
-        <View style={styles.content}>
-          <TouchableOpacity onPress={() => this.openImage(bill.url)}>
-            <Image
-              style={styles.thumbnail}
-              source={{ uri: bill.thumbnailUrl }}
-            />
-          </TouchableOpacity>
-        </View>
-        <View style={styles.content}>
-          <TextField heading="Date" text={formattedDate} />
-          <TextField heading="Amount" text={`$${bill.amount}`} />
-          <View style={styles.row}>
-            <TextField heading="Status" text={formattedStatus} />
-            <View style={styles.tooltip}>
-              <StatusTooltip status={bill.status} />
-            </View>
+  return (
+    <View style={styles.container}>
+      <View style={styles.content}>
+        <TouchableOpacity onPress={() => setBillFullScreen(true)}>
+          <Image style={styles.thumbnail} source={{ uri: bill.thumbnailUrl }} />
+        </TouchableOpacity>
+      </View>
+      <View style={styles.content}>
+        <TextField heading="Date" text={formattedDate} />
+        <TextField heading="Amount" text={`$${bill.amount}`} />
+        <View style={styles.row}>
+          <TextField heading="Status" text={formattedStatus} />
+          <View style={styles.tooltip}>
+            <StatusTooltip status={bill.status} />
           </View>
         </View>
-        <View>
-          <BillFullScreenModal
-            image={this.state.image}
-            isBillFullScreen={this.state.isBillFullScreen}
-            close={this.closeImage}
-          />
-        </View>
       </View>
-    );
-  }
-}
+      <View>
+        <BillFullScreenModal
+          image={bill.url}
+          isBillFullScreen={isBillFullScreen}
+          close={closeModal}
+        />
+      </View>
+    </View>
+  );
+};
+
+export default BillItem;
 
 const styles = StyleSheet.create({
   container: {
     borderWidth: 1,
     flexDirection: 'row',
     height: 90,
-    justifyContent: 'space-around',
+    justifyContent: 'flex-start',
   },
   thumbnail: {
     width: 70,
@@ -76,15 +63,16 @@ const styles = StyleSheet.create({
   },
   content: {
     justifyContent: 'center',
+    padding: 10
   },
   row: {
     flexDirection: 'row',
   },
-  heading: {
-    fontWeight: 'bold',
-  },
   tooltip: {
     alignSelf: 'flex-start',
+    paddingLeft: 5,
+    height: 20,
+    width: 30,
   },
 });
 
